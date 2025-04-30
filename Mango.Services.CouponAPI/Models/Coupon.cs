928365e7addd764f6375dbdc1fef72f0c78e7fab
r134p694_a <rithvij2019@gmail.com>
2025-04-30 10:48:34 -0500

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int CouponId { get; set; }
+        [Required]
+        public string CouponCode {get ; set;}
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
